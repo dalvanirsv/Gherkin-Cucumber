9718712be5ed288175a7ddcfe5ff5b9a337db212
dalvanirsv <eeep.dalvani.vieira@gmail.comm>
2022-09-19 21:16:17 -0300

    add-conter

--- Cucumber/src/test/java/LearnCucumber.java
+++ Cucumber/src/test/java/LearnCucumber.java
@@ -1,42 +1,42 @@
+import org.junit.Assert;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class LearnCucumber {
-	@Given("que criei o arquivo corretamente")
-	public void que_criei_o_arquivo_corretamente() {
-	  System.out.println("qualquer coisa");
+	public class LearnCucumber {
+		@Given("que criei o arquivo corretamente")
+			public void que_criei_o_arquivo_corretamente() {
+				System.out.println("qualquer coisa");
 	}
 
-	@When("executa-lo")
-	public void execut_lo() {
+		@When("executa-lo")
+			public void execut_lo() {
 	   
 	}
-	
-			
-	@Then("a especificacao deve finalizar com sucesso")
-	public void a_especifica_o_deve_finalizar_com_sucesso() {
+		
+		@Then("a especificacao deve finalizar com sucesso")
+			public void a_especifica_o_deve_finalizar_com_sucesso() {
 	  
 	}
 	
-	////////////////////////////////////////////////////////////////////////////////////////////
-	
-	private int contador = 0;
-	@Given("que o valor do contador e {int}")
-	public void que_o_valor_do_contador_e(Integer int1) throws Throwable {
-		 contador = int1;
+		private int contador = 0;
+		@Given("que o valor do contador e {int}")
+			public void que_o_valor_do_contador_e(Integer int1) throws Throwable {
+				contador = int1;
 	}
 
-	@When("eu incrementar em {int}")
-	public void eu_incrementar_em(Integer int1) throws Throwable{
-		contador = contador + int1;
+		@When("eu incrementar em {int}")
+			public void eu_incrementar_em(Integer int1) throws Throwable{
+				contador = contador + int1;
 	}
 
-	@Then("o valor do contador sera {int}")
-	public void o_valor_do_contador_sera(Integer int1) {
-		System.out.println(int1);
-		System.out.println(int1 == contador);
-	}
+		@Then("o valor do contador sera {int}")
+			public void o_valor_do_contador_sera(Integer int1) {
+			Assert.assertEquals(int1, Integer.valueOf(contador));
+				
+				
+				}
 	}
 
 	
